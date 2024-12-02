841c74dd8ebcfbc1ba36e7e5063422ce8d43b5df
Bren <bpgit503@gmail.com>
2024-12-02 12:34:46 +0100

    deleted duplicated association

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -17,6 +17,7 @@ public class Book {
     inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
+
     public Set<Author> getAuthors() {
         return authors;
     }
